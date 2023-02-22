385241520936c6cf833e9846f3fe57f4f885c61e
Bless561 <blessnji52@gmail.com>
2023-02-22 03:14:39 -0800

    Print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_sign - Determines if the input number
@@ -11,19 +11,20 @@
  */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
 	}
-	else if(n = 0)
+	else
 	{
 		_putchar(48);
 		return (0);
 	}
+	_putchar('\n');
 }
