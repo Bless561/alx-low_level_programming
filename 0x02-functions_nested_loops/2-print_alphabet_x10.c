124834001bcb37d6f422c4eb8df3734d37c939a4
Bless561 <blessnji52@gmail.com>
2023-02-22 03:11:16 -0800

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@
 void print_alphabet_x10(void)
 {
 	int a, b;
-	for(a = 0; a <= 9; a++)
+
+	for (a = 0; a <= 9; a++)
 	{
-		for(b = 97; b <= 122; b++)
+		for (b = 97; b <= 122; b++)
 		{
 			_putchar(b);
 		}
-	_putchar("\n");
+	_putchar('\n');
 	}
-	return (0);
 }
