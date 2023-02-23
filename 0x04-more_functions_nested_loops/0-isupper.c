8dc62ac5585fad118bb03b2fc943d6e0a6f5f2ec
Bless561 <blessnji52@gmail.com>
2023-02-23 05:00:30 -0800

    is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * isupper checks if input is an uppercase
+ *
+ * returns 1 if true and 0 if false
+ */
+int _isupper(int c)
+{
+	if(c >= 65 && c <=90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
